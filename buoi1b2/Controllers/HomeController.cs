79d762a669200baa7f02a397f56ce69fd59251bd
khoanguyen3624 <khoanguyen3624@gmail.com>
2025-09-26 23:10:30 +0700

    Add project files.

---
+++ buoi1b2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace buoi1b2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult baitap1()
+        {
+            
+
+            return View();
+        }
+        public ActionResult baitap2()
+        {
+
+
+            return View();
+        }
+        public ActionResult baitap3()
+        {
+
+
+            return View();
+        }
+        public ActionResult baitap4()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ buoi1b2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="buoi1b2.MvcApplication" Language="C#" %>
